9e9911827200d3d7b70140842f173a2b776ec0ba
DeveloperIgorR <bratca@inbox.ru>
2021-07-23 01:35:59 +0300

    Fix22

--- src/AlbumList/AlbumList.jsx
+++ src/AlbumList/AlbumList.jsx
@@ -1,30 +1,15 @@
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import { instance } from '../http/http'
 import l from './AlbumList.module.css'
 
 const AlbumList = (props) => {
-const[pageNumber,setPageNumber] = useState(1)
-const [newPortion,setNewPortion] = useState([])
-
-  const limit = 9
-  async function getNewPhotos(pageNumber,limit){
-    const respons = await instance.get(`photos?_page=${pageNumber}&_limit=${limit}`)
-    setNewPortion(respons.data)
-} 
-
-let setPortion = (id)=>{
-    setPageNumber(id)
-    getNewPhotos(pageNumber,limit)
-    props.getNewPortion(newPortion)
-}
 
 const arr = [1,2,3,4,5]
     return (
         <div>
             <div className={l.pagination}>
                 <h2> Альбом с фотографиями</h2>
-                {arr.map((id)=><button id={arr.index+1} onClick={(id)=>setPortion(id)} >{id}</button>)}
+                {arr.map((id)=><button id={arr.index+1} onClick={(id)=>props.setPortion(id)} >{id}</button>)}
                
             </div>
 
--- src/album/album.jsx
+++ src/album/album.jsx
@@ -1,28 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import AlbumForm from '../AlbumForm/AlbumForm'
 import AlbumList from '../AlbumList/AlbumList'
+import { instance } from '../http/http'
 
 const Album = () => {
     const [files, setFiles] = useState([])
-    const [page,setPage] = useState(1)
-    const limit = 9
+    const[pageNumber,setPageNumber] = useState(1)
     useEffect(()=>{
-        getNewPortion(page,limit)
+        getNewPhotos(pageNumber,limit)
     },[])
+    const limit = 9
+
+    async function getNewPhotos(pageNumber,limit){
+        const respons = await instance.get(`photos?_page=${pageNumber}&_limit=${limit}`)
+        setFiles(respons.data)
+    } 
+    
+    let setPortion = (id)=>{
+        setPageNumber(id)
+        getNewPhotos(pageNumber,limit)
+    }
   
     function createFiles (newFile)  {
       setFiles([...files,newFile])
         
-    }
-    
-    function getNewPortion (newPortion){
-       setFiles([...files,newPortion])
-    }
+    }    
+   
     return (
         <div>
             <div>
                 <AlbumForm createFiles={createFiles} />
-                <AlbumList getNewPortion={getNewPortion} files={files}/>
+                <AlbumList setPortion={setPortion} files={files}/>
             </div>
             
         </div>
