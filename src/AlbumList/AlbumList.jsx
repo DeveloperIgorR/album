c211554d2c3001b42ea99e081ad6e210f79b023a
DeveloperIgorR <bratca@inbox.ru>
2021-07-23 22:46:13 +0300

    Fix22

--- src/AlbumList/AlbumList.jsx
+++ src/AlbumList/AlbumList.jsx
@@ -1,18 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 import l from './AlbumList.module.css'
 
 const AlbumList = (props) => {
 
-const arr = [1,2,3,4,5]
     return (
         <div>
-            <div className={l.pagination}>
-                <h2> Альбом с фотографиями</h2>
-                {arr.map((id)=><button id={arr.index+1} onClick={(id)=>props.setPortion(id)} >{id}</button>)}
-               
-            </div>
-
+            
             <div className={l.list}>
 
                 {props.files.map(discription =>
--- src/album/album.jsx
+++ src/album/album.jsx
@@ -2,37 +2,44 @@ import React, { useEffect, useState } from 'react'
 import AlbumForm from '../AlbumForm/AlbumForm'
 import AlbumList from '../AlbumList/AlbumList'
 import { instance } from '../http/http'
+import l from '../AlbumList/AlbumList.module.css'
 
 const Album = () => {
     const [files, setFiles] = useState([])
-    const[pageNumber,setPageNumber] = useState(1)
-    useEffect(()=>{
-        getNewPhotos(pageNumber,limit)
-    },[pageNumber])
+    const [pageNumber, setPageNumber] = useState(1)
     const limit = 9
+    useEffect(() => {
+        getNewPhotos()
+    }, [])
+    
 
-    async function getNewPhotos(pageNumber,limit){
+    async function getNewPhotos() {
         const respons = await instance.get(`photos?_page=${pageNumber}&_limit=${limit}`)
         setFiles(respons.data)
-    } 
-    
-    let setPortion = (id)=>{
-        setPageNumber(id)
-        getNewPhotos(pageNumber,limit)
     }
-  
-    function createFiles (newFile)  {
-      setFiles([...files,newFile])
-        
-    }    
-   
+
+    let setPortion = (id) => {
+        setPageNumber(id)
+        getNewPhotos()
+    }
+
+    function createFiles(newFile) {
+        setFiles([...files, newFile])
+
+    }
+
+    const arr = [1,2,3,4,5]
     return (
         <div>
             <div>
                 <AlbumForm createFiles={createFiles} />
-                <AlbumList setPortion={setPortion} files={files}/>
+                <div className={l.pagination}>
+                    <h2> Альбом с фотографиями</h2>
+                    {arr.map((id) => <button id={arr.index + 1} onClick={(id) =>setPortion(id)} >{id}</button>)}
+                </div>
+                <AlbumList setPortion={setPortion} files={files} />
             </div>
-            
+
         </div>
     )
 }
