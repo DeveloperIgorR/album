fba3f67c88fc389e0742ab97d7bd224d113cfc72
DeveloperIgorR <bratca@inbox.ru>
2021-07-30 01:31:49 +0300

    Fix43

--- src/albums/Albums.jsx
+++ src/albums/Albums.jsx
@@ -1,24 +1,32 @@
 import React, { useEffect, useState } from "react"
+import { useHistory, useParams } from "react-router-dom"
 import { instance } from "../http/http"
 
 const Albums = () => {
 
     const [album, setAlbum] = useState([])
-    
+    const params = useParams()
+    const router = useHistory()
     useEffect(() => {
         getAlbum()
     }, [])
 
     async function getAlbum() {
-        const respons = await instance.get('photos')
+        const respons = await instance.get('photos'+ params.albumId)
         setAlbum(respons.data)
-    }
-
+        console.log(respons.data)
+        
+    }  
+  
     return (
         <div>
-            {album.map(params => {
-                return <div>{params.albumId}</div>
+            {album.map(images => {
+                return <img  src={images.url}/>
             })}
+            <div>
+              <button onClick={() => router.push('/')}> back</button>
+            </div>
+            
         </div>
     )
 }
