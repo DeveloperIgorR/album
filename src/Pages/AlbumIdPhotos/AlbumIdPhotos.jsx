37a4140b1fa00432a055ae1b582fed710e6de9bd
DeveloperIgorR <bratca@inbox.ru>
2021-07-31 23:14:52 +0300

    Fix49

--- src/Pages/AlbumIdPhotos/AlbumIdPhotos.jsx
+++ src/Pages/AlbumIdPhotos/AlbumIdPhotos.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import { instance } from '../../http/http'
 import a from './AlbumIdPhotos.module.css'
 
@@ -7,6 +8,7 @@ const AlbumIdPhotos = () => {
     useEffect(() => {
         getAlbumId()
     }, [])
+    const router = useHistory()
 
     async function getAlbumId() {
         const respons = await instance.get('albums/')
@@ -19,7 +21,7 @@ const AlbumIdPhotos = () => {
             {albumId.map((name) => {
                 return (
                     <div className={a.album} >
-                        <span ><h3>{name.title}</h3></span><button>Открыть</button>
+                        <span ><h3>{name.title}</h3></span><button onClick={() => router.push('/album/albumId')}>Открыть</button>
                     </div>
                 )
             })}
